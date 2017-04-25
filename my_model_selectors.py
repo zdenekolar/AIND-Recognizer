09833d413a4643979a267350c15508aa9f5debe9
Zdenek Kolar <Zdenek Kolar>
2017-04-25 21:44:35 +0800

    Improve my_model_selectors.py and my_recognizer.py

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -34,7 +34,7 @@ class ModelSelector(object):
     def base_model(self, num_states):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
@@ -61,39 +61,85 @@ class SelectorConstant(ModelSelector):
         return self.base_model(best_num_components)
 
 
-# class SelectorBIC(ModelSelector):
-#     """ select the model with the lowest Baysian Information Criterion(BIC) score
-#
-#     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
-#     Bayesian information criteria: BIC = -2 * logL + p * logN
-#     """
-#
-#     def select(self):
-#         """ select the best model for self.this_word based on
-#         BIC score for n between self.min_n_components and self.max_n_components
-#
-#         :return: GaussianHMM object
-#         """
-#         warnings.filterwarnings("ignore", category=DeprecationWarning)
-#
-#         # TODO implement model selection based on BIC scores
-#         raise NotImplementedError
-#
-#
-# class SelectorDIC(ModelSelector):
-#     ''' select best model based on Discriminative Information Criterion
-#
-#     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
-#     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
-#     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-#     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-#     '''
-#
-#     def select(self):
-#         warnings.filterwarnings("ignore", category=DeprecationWarning)
-#
-#         # TODO implement model selection based on DIC scores
-#         raise NotImplementedError
+class SelectorBIC(ModelSelector):
+    """ select the model with the lowest Baysian Information Criterion(BIC) score
+
+    http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+    Bayesian information criteria: BIC = -2 * logL + p * logN
+    """
+
+    def select(self):
+        """ select the best model for self.this_word based on
+        BIC score for n between self.min_n_components and self.max_n_components
+
+        :return: GaussianHMM object
+        """
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        best_score = float('inf')
+        best_model = None
+
+        for number in range(self.min_n_components, self.max_n_components):
+
+            try:
+                model = self.base_model(number)
+                logL = model.score(self.X, self.lengths)
+                transition_prob = model.transmat_
+                start_prob = model.startprob_
+                p = len(transition_prob) + len(start_prob)
+                N = len(self.X)
+
+                BIC = -2 * logL + p * np.log(N)
+
+                if BIC < best_score:
+                    best_model = model
+            except:
+                pass
+
+        return best_model
+
+
+class SelectorDIC(ModelSelector):
+    ''' select best model based on Discriminative Information Criterion
+
+    Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
+    Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+    DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    '''
+
+    def select(self):
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        best_model = None
+        best_score = float('-inf')
+
+        keys = list(self.hwords.keys())
+        other_keys = keys.remove(self.this_word)
+
+        sum_logs = 0
+
+        for number in range(self.min_n_components, self.max_n_components):
+            model = None
+            score = 0
+            try:
+                # This word model
+                model = self.base_model(number)
+                logL_word = model.score(self.X, self.lengths)
+
+                # Other words models
+                for key in other_keys:
+                    X, lengths = self.hwords[key]
+                    logL = model.score(X, lengths)
+                    sum_logs.append(logL)
+
+                score = logL_word - 1/(len(keys) - 1) * sum_logs
+            except:
+                pass
+            if score > best_score:
+                best_model = model
+
+        return best_model
 
 
 class SelectorCV(ModelSelector):
@@ -102,11 +148,26 @@ class SelectorCV(ModelSelector):
     '''
 
     def select(self):
-        # warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
+        best_model = None
+        max_score = float('-inf')
+        split_method = KFold(min(3, len(self.sequences[0])))
+
         for number in range(self.min_n_components, self.max_n_components):
-            model = self.base_model(number)
-            print(model._compute_log_likelihood(self.words))
-        return None
+            set_score = []
+            for cv_train, cv_test in split_method.split(self.sequences[0]):
+                try:
+                    X_train, lengths_train = combine_sequences(cv_train, self.sequences)
+                    X_test, lengths_test = combine_sequences(cv_train, self.sequences)
 
+                    model = self.base_model(number)
+                    model.fit(X_train, lengths_train)
+                    logL = model.score(X_test, lengths_test)
+                    set_score.append(logL)
+
+                    if np.mean(set_score) > max_score:
+                        best_model = model
+                except:
+                    pass
+        return best_model
--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,24 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    num_items = test_set.num_items
+
+    for word_id in range(num_items):
+        X, lengths = test_set.get_item_Xlengths(word_id)
+        probs = dict()
+        for train_word, model in models.items():
+            try:
+                # print(train_word)
+                # print(word_id, X, lengths)
+                score = model.score(X, lengths)
+                # print('Score ', score)
+                probs[train_word] = score
+            except:
+                pass
+        probabilities.append(probs)
+        guess = max(probs, key=lambda i: probs[i])
+        guesses.append(guess)
+
+    return probabilities, guesses
+
